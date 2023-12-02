54a263b5b02e3d25bb64e462aa8529b89972484f
Dylan Borchert <dylanbor01@gmail.com>
2023-12-02 00:20:36 -0700

    part 2 setup

---
+++ day0.py
@@ -0,0 +1,18 @@
+from template import Template
+
+
+class day0(Template):
+    def __init__(self):
+        self.day = 0
+        super().__init__(self.day)
+
+        self.set_data(self.get_data().split("\n")[:-1])
+
+    def part1(self):
+        return self.get_data()
+
+    def part2(self):
+        pass
+
+
+print(day0())
