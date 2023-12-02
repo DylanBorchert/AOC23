18b1f41b7d9bf2ba08270871064406d154a73d33
Dylan Borchert <dylanbor01@gmail.com>
2023-12-02 14:54:52 -0700

    day 2 done

--- day2.py
+++ day2.py
@@ -9,10 +9,41 @@ class day2(Template):
         self.set_data(self.get_data().split("\n")[:-1])
 
     def part1(self):
-        return self.get_data()
+        possible = 0
+        data = [x.strip().split(": ")[1].split("; ") for x in self.get_data()]
+
+        for i, x in enumerate(data):
+            for y in x:
+                colors = {"red": 0, "green": 0, "blue": 0}
+                y = y.split(", ")
+                for z in y:
+                    n, c = z.split(" ")
+                    colors[c] = int(n)
+                if colors["red"] > 12 or colors["green"] > 13 or colors["blue"] > 14:
+                    break
+            else:
+                possible += i + 1
+        return possible
 
     def part2(self):
-        pass
+        possible = 0
+        data = [x.strip().split(": ")[1].split("; ") for x in self.get_data()]
+
+        for i, x in enumerate(data):
+            print(x)
+            max_color = {"red": 0, "green": 0, "blue": 0}
+            for y in x:
+                colors = {"red": 0, "green": 0, "blue": 0}
+                y = y.split(", ")
+                for z in y:
+                    n, c = z.split(" ")
+                    colors[c] = int(n)
+                for c in colors:
+                    max_color[c] = max(max_color[c], colors[c])
+            else:
+                possible += max_color["red"] * \
+                    max_color["green"] * max_color["blue"]
+        return possible
 
 
 print(day2())
