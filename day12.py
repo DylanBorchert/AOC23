a48d300b3fa2fc9dd4b6c92689f6587beeb4fbc6
Dylan Borchert <dylanbor01@gmail.com>
2023-12-15 10:26:28 -0700

    day 12 done and day 13 started

--- day12.py
+++ day12.py
@@ -1,28 +1,64 @@
 from template import Template
-from itertools import combinations
+import numpy as np
+
+cache = {}
+
+
+def count(springs, groups):
+    if springs == "":
+        return 1 if groups == () else 0
+
+    if groups == ():
+        return 0 if "#" in springs else 1
+
+    key = (springs, groups)
+
+    if key in cache:
+        return cache[key]
+
+    result = 0
+
+    if springs[0] in ".?":
+        result += count(springs[1:], groups)
+
+    if springs[0] in "#?":
+        if groups[0] <= len(springs) and "." not in springs[:groups[0]] and (groups[0] == len(springs) or springs[groups[0]] != "#"):
+            result += count(springs[groups[0] + 1:], groups[1:])
+
+    cache[key] = result
+    return result
 
 
 class day0(Template):
     def __init__(self):
         self.day = 12
-        super().__init__(self.day, useFile=True)
+        super().__init__(self.day, useFile=False)
         self.set_data(self.get_data().strip().split("\n"))
 
     def part1(self):
+        total = 0
         for i, springs in enumerate(self.get_data()):
-            spring, condition = springs.split(" ")
-            condition = [int(x) for x in condition.split(",")]
-            unkowns = spring.count("?")
-            broken = spring.count("#")
-            opperational = len(spring) - broken
+            springs, groups = springs.split(" ")
+            groups = tuple(map(int, groups.split(",")))
 
-            unknown_indexs = []
-            for i, ch in enumerate(spring):
-                if ch == "?":
-                    unknown_indexs.append(i)
+            total += count(springs, groups)
+        return total
 
     def part2(self):
-        pass
+        total = 0
+        for i, springs in enumerate(self.get_data()):
+            springs, groups = springs.split(" ")
+            groups = tuple(map(int, groups.split(",")))
+
+            groups = (groups * 5)
+
+            new_springs = ""
+            for i in range(5):
+                new_springs = new_springs + springs + "?"
+            springs = new_springs[:-1]
+
+            total += count(springs, groups)
+        return total
 
 
 print(day0())
